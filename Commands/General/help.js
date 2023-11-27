c07d3054ffcd925ab7c9f75cfdb1769910b4afd2
felix2336 <felix-pra@gmx.de>
2023-11-27 20:22:50 +0100

    new

---
+++ Commands/General/help.js
@@ -0,0 +1,45 @@
+const { CommandInteraction, Client, EmbedBuilder, ApplicationCommandOptionType } = require('discord.js')
+const fs = require('fs')
+
+module.exports = {
+    name: 'help',
+    description: 'Führe diesen Befehl aus, wenn du Hilfe brauchst',
+    permission: 'SendMessages',
+    options: [
+        {
+            name: 'casino',
+            description: 'Lasse dir alle Befehle zum Casino System anzeigen',
+            type: ApplicationCommandOptionType.Subcommand
+        },
+    ],
+
+    /**
+     * @param {CommandInteraction} interaction 
+     * @param {Client} client 
+     */
+
+    async execute(interaction, client){
+        const subcommand = interaction.options.getSubcommand()
+
+        switch(subcommand){
+            case 'casino': {
+                let fieldArray = [];
+                const folder = fs.readdirSync('../../Commands/Casino')
+                for(const file of folder){
+                    const module = require(file)
+                    const name = module.name
+                    const value = module.description
+                    fields.push({name, value})
+                }
+                const embed = new EmbedBuilder({
+                    title: '**Casino Hilfe**',
+                    description: 'Hier findest du alle Befehle des Casino Systems',
+                    fields: fieldArray,
+                    color: 0xffc400
+                })
+
+                interaction.reply({embeds: [embed]})
+            }
+        }
+    }
+}
