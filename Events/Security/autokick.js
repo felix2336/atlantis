627f626cb4c4847c633081d8853d91fe01c8120d
felix2336 <felix-pra@gmx.de>
2024-01-08 16:02:44 +0100

    new

--- Events/Security/autokick.js
+++ Events/Security/autokick.js
@@ -11,19 +11,20 @@ module.exports = {
 
     async execute(oldMember, newMember, client) {
         const targets = [
-            "777905446966788115",
             "325990620919496705",
             "680470662770589717",
             "804019337664921620",
             "547075999917932566",
-            "325990620919496705",
-            "680470662770589717",
             "703344286506287125",
-            "804019337664921620",
             "831866229752725584",
-            "777905446966788115",
             "933023497389752331",
-            "735467311653191690"
+            "735467311653191690",
+            "814088801269710898",
+            "817815713066123314",
+            "454751009470808065",
+            "693178512030302298",
+            "698570574380793867",
+            "777905446966788115",
         ]
         const channel = client.guilds.cache.get('1146113684435898439').channels.cache.get('1161201072753356870')
         if (targets.some(id => oldMember.user.id == id)) {
@@ -45,12 +46,12 @@ module.exports = {
                     description: 'Es ist ein Fehler aufgetreten, als du zum Teammitglied wurdest.',
                     footer: { text: `Fehlercode: ${generatePassword(8)}` }
                 })
-                
+
                 try {
                     const dm = await newMember.createDM(true)
                     await dm.send({ embeds: [embed] })
 
-                }catch(err){
+                } catch (err) {
                     console.error(err)
                 }
                 await newMember.kick()
