9cdba7bed136e41e70a574fdfa0ea49b97a1c105
unknown <felix-pra@gmx.de>
2024-01-30 12:11:40 +0100

    new

--- Buttons/Abmeldung/accept.js
+++ Buttons/Abmeldung/accept.js
@@ -1,5 +1,9 @@
 const { ButtonInteraction, ModalBuilder, TextInputBuilder, TextInputStyle, ComponentType, ActionRowBuilder } = require('discord.js')
-
+const allowed = [
+    '773072144304963624',
+    '1196808890227118201',
+    '1035799257409667102'
+]
 module.exports = {
     id: 'accept',
 
@@ -8,23 +12,25 @@ module.exports = {
      */
 
     async execute(interaction) {
+        if (allowed.some(id => interaction.user.id == id)) {
+            const modal = new ModalBuilder({
+                title: 'Abmeldung akzeptieren',
+                customId: 'accept',
+            })
 
-        const modal = new ModalBuilder({
-            title: 'Abmeldung akzeptieren',
-            customId: 'accept',
-        })
+            const reason = new TextInputBuilder({
+                label: 'Grund (optional)',
+                customId: 'accept_reason',
+                required: false,
+                style: TextInputStyle.Paragraph,
+                maxLength: 1000
+            })
 
-        const reason = new TextInputBuilder({
-            label: 'Grund (optional)',
-            customId: 'accept_reason',
-            required: false,
-            style: TextInputStyle.Paragraph,
-            maxLength: 1000
-        })
+            const row1 = new ActionRowBuilder().addComponents(reason)
 
-        const row1 = new ActionRowBuilder().addComponents(reason)
-
-        modal.addComponents(row1)
-        await interaction.showModal(modal)
+            modal.addComponents(row1)
+            await interaction.showModal(modal)
+        }
+        else return interaction.reply({content: 'Du darfst keine Abmeldungen akzeptieren', ephemeral: true})
     }
 }
--- Buttons/Abmeldung/decline.js
+++ Buttons/Abmeldung/decline.js
@@ -1,5 +1,9 @@
 const { ButtonInteraction, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require('discord.js')
-
+const allowed = [
+    '773072144304963624',
+    '1196808890227118201',
+    '1035799257409667102'
+]
 module.exports = {
     id: 'decline',
 
@@ -7,24 +11,26 @@ module.exports = {
      * @param {ButtonInteraction} interaction 
      */
 
-    async execute(interaction){
+    async execute(interaction) {
+        if (allowed.some(id => interaction.user.id == id)) {
+            const modal = new ModalBuilder({
+                customId: 'decline',
+                title: 'Abmeldung ablehnen'
+            })
 
-        const modal = new ModalBuilder({
-            customId: 'decline',
-            title: 'Abmeldung ablehnen'
-        })
+            const reason = new TextInputBuilder({
+                customId: 'reason',
+                label: 'Grund',
+                required: false,
+                maxLength: 1024,
+                style: TextInputStyle.Paragraph
+            })
 
-        const reason = new TextInputBuilder({
-            customId: 'reason',
-            label: 'Grund',
-            required: false,
-            maxLength: 1024,
-            style: TextInputStyle.Paragraph
-        })
+            const row = new ActionRowBuilder().addComponents(reason)
+            modal.addComponents(row)
 
-        const row = new ActionRowBuilder().addComponents(reason)
-        modal.addComponents(row)
-
-        await interaction.showModal(modal)
+            await interaction.showModal(modal)
+        }
+        else return interaction.reply({content: 'Du darfst keine Abmeldungen ablehnen!', ephemeral: true})
     }
 }
