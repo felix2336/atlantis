1aa3c236b265537e1fa8262b6fa4b6687be02c55
felix_2336 <felix-pra@gmx.de>
2024-04-17 21:10:50 +0200

    new

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 export enum Channels {
     teamliste = "1173357582933573722",
     warn = "1160607902210470009",
-    suggestion = "1192541528795652106"
+    suggestion = "1230234007854120960"
 }
 export enum Roles {
     staff = '1156298949301379212'
