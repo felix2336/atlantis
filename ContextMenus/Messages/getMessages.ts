09790808fb812af7cbb8ba054f829b225907c22b
felix_2336 <felix-pra@gmx.de>
2024-04-18 16:43:16 +0200

    new

--- ContextMenus/Messages/getMessages.ts
+++ ContextMenus/Messages/getMessages.ts
@@ -12,8 +12,9 @@ export default {
         const target = interaction.targetMember as GuildMember
         if (!member.roles.cache.has('1156298949301379212')) return interaction.reply({ content: 'Du bist nicht im Serverteam und darfst diesen Befehl nicht nutzen', ephemeral: true })
         const DB = JSON.parse(readFileSync('./JSON/messages.json', 'utf8')) as MessageUser[]
-        const User = DB.find(u => u.userid == target.user.id)
-        if (!User) return interaction.reply({ content: 'Dieser Nutzer hat keinen Eintrag in der Datenbank.', ephemeral: true })
+        const UserData = DB.find(u => u.userid == target.user.id)
+        if (!UserData) return interaction.reply({ content: 'Dieser Nutzer hat keinen Eintrag in der Datenbank.', ephemeral: true })
+        const User = new MessageUser().assignData(UserData)
 
         interaction.reply({ content: `${target} hat in dieser Woche bereits ${User.getTotalMessages()} Nachrichten gesendet!`, ephemeral: true })
     }
