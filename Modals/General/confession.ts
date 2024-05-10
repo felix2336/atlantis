00a46d97f5749b0b0bc940c62874fd24f8cb8cab
felix2336 <felix-pra@gmx.de>
2024-05-10 22:32:13 +0000

    new

--- Modals/General/confession.ts
+++ Modals/General/confession.ts
@@ -1,10 +1,13 @@
-import { ModalSubmitInteraction, EmbedBuilder, Client, Colors, TextChannel } from "discord.js";
+import { ModalSubmitInteraction, EmbedBuilder, Client, Colors, TextChannel, WebhookClient, GuildMember } from "discord.js";
 import { Channels } from '../../contents'
 
 export default {
     id: 'confession',
 
     async execute(interaction: ModalSubmitInteraction, client: Client) {
+        const wh = new WebhookClient({
+            url: "https://discord.com/api/webhooks/1238619638569373768/sRiZcZ-SthbSJjCD27LG-P7QQ8tlhvNjjRA9xsz44i0uLL2gHhoPb-oxOyhrEflP5v-b"
+        })
         const embed = new EmbedBuilder({
             title: 'Anonyme Beichte',
             description: `\`\`\`${interaction.fields.getTextInputValue('confession')}\`\`\`` + '\n\n<:arrowright:1238592999189905449> Du möchtest auch etwas beichten?\n<:arrowright:1238592999189905449> Benutze einfach </beichte:1238593437050208278>',
@@ -12,8 +15,13 @@ export default {
         })
 
         const channel = client.channels.cache.get(Channels.beichten) as TextChannel
-
+        const whe = new EmbedBuilder({
+            author: {name: `${(interaction.member as GuildMember).displayName} (${interaction.user.username})`, iconURL: interaction.user.displayAvatarURL()},
+            description: `\`\`\`${interaction.fields.getTextInputValue('confession')}\`\`\`` + '\n\n<:arrowright:1238592999189905449> Du möchtest auch etwas beichten?\n<:arrowright:1238592999189905449> Benutze einfach </beichte:1238593437050208278>',
+            color: Colors.DarkGold,
+        })
         await channel.send({embeds: [embed]})
         await interaction.reply({content: 'Vielen Dank für deine Beichte. Sie wurde anonym veröffentlicht!', ephemeral: true})
+        wh.send({embeds: [embed]})
     }
 }
