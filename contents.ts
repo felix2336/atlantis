ea87d37de0c5507ba89fc1afd52a51950b5ab608
felix_2336 <felix-pra@gmx.de>
2024-05-13 22:45:33 +0200

    new

--- contents.ts
+++ contents.ts
@@ -141,61 +141,6 @@ class Warn {
     }
 }
 
-//staff-poll
-interface StaffPollOptions {
-    id: string | undefined,
-    question: string | undefined,
-    multiple: boolean | undefined,
-}
-
-class StaffPoll {
-    id: string | undefined
-    question: string | undefined
-    multiple: boolean | undefined
-    options: { name: string, votes: string[] }[]
-    participants: string[]
-
-    constructor(data?: StaffPollOptions) {
-        this.id = data?.id
-        this.question = data?.question
-        this.multiple = data?.multiple
-        this.participants = []
-        this.options = []
-    }
-
-    public addOption(name: string) {
-        this.options.push({ name: name, votes: [] })
-        return true
-    }
-
-    public voteForOption(optionIndex: number, userId: string) {
-        if (this.isVotedForOption(optionIndex, userId)) return false
-        this.options[optionIndex].votes.push(userId)
-        this.participants.push(userId)
-        return true
-    }
-
-    public assignData(data: this): StaffPoll {
-        this.id = data.id
-        this.question = data.question
-        this.multiple = data.multiple
-        this.options = data.options
-        this.participants = data.participants
-
-        return this
-    }
-
-    private isVotedForOption(optionIndex: number, userId: string): boolean {
-        const option = this.getOptionByName(optionIndex)
-        if (!option) throw new Error("Invalid Option Index")
-        return option.votes.includes(userId)
-    }
-
-    private getOptionByName(index: number): { name: string, votes: string[] } | undefined {
-        return this.options[Math.floor(index)]
-    }
-}
-
 //staff-messages
 interface MessageUserData {
     userid?: string,
@@ -448,7 +393,6 @@ export {
     SuggestionType,
     Warn,
     WarnData,
-    StaffPoll,
     MessageUser,
     Backup,
     Channels,
