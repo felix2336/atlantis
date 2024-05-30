272118a0405702848b8b71b9b070c5aaa9b3f0f3
felix_2336 <felix-pra@gmx.de>
2024-05-30 23:41:01 +0200

    new

--- Events/General/join.ts
+++ Events/General/join.ts
@@ -1,10 +1,21 @@
-import { GuildMember, Client, EmbedBuilder, Colors, ActionRowBuilder, ButtonBuilder, TextChannel } from 'discord.js'
+import { GuildMember, Client, EmbedBuilder, Colors, ActionRowBuilder, ButtonBuilder, TextChannel, AttachmentBuilder } from 'discord.js'
 import { Channels } from '../../contents'
+import { profileImage } from 'discord-arts'
 export default {
     name: 'guildMemberAdd',
 
     async execute(member: GuildMember) {
         const channel = member.guild.channels.cache.get(Channels.welcome) as TextChannel
+
+        const buffer = await profileImage(member.user.id, {
+            badgesFrame: true,
+            customTag: 'Willkommen',
+            presenceStatus: member.presence?.status,
+        })
+
+        const attachment = new AttachmentBuilder(buffer, { name: 'image.png' })
+
+        
         const embed = new EmbedBuilder({
             title: "Willkommen",
             description: `Hallo ${member} und ein herzliches Willkommen auf unserem Server! 👋\nWir freuen uns, dass du unserer Community beitreten bist ❤️\n\n🤝 Falls du Lust hast, kannst du dich in <#1182194022010322975> unserer Community vorstellen - teile uns gerne etwas über dich, deine Interessen und was dich hierhin führt mit.\n\n📜 Bevor du loslegen kannst, nimm dir bitte einen Moment Zeit, um unsere Serverregeln durchzulesen. Wir möchten, dass jeder hier eine tolle Zeit hat, und die Einhaltung der Richtlinien trägt dazu bei, ein positives Umfeld für alle zu schaffen.\n\nWir freuen uns dass du unserem Server beigetreten bist. 🎉\n\nWir sind jetzt **136 Mitglieder**\n\nMit freundlichen Grüßen:\n**Das Server-Team**`,
@@ -15,7 +26,7 @@ export default {
                     value: "- Unsere Regeln in <#1146113685962625128> \n- Selfroles in <#1148123116590071828> \n- Support in <#1153007107688386752>",
                 }
             ]
-        })
+        }).setImage('attachment://image.png')
 
         const row = new ActionRowBuilder<ButtonBuilder>().addComponents([
             new ButtonBuilder({
@@ -25,6 +36,6 @@ export default {
             })
         ])
 
-        await channel.send({ content: `${member}`, embeds: [embed], components: [row] })
+        await channel.send({ content: `${member}`, embeds: [embed], components: [row], files: [attachment] })
     }
 }
