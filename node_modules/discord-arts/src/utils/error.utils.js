48f4bee7563731a05ac353ffaa2ef61fd9fe3fe1
felix_2336 <felix-pra@gmx.de>
2024-06-29 02:07:44 +0200

    new

--- node_modules/discord-arts/src/utils/error.utils.js
+++
@@ -1,9 +0,0 @@
-class DiscordArtsError extends Error {
-  constructor(message, info) {
-    const { userId, packageVersion } = info;
-    super(message);
-    this.userId = userId;
-  }
-}
-
-module.exports = DiscordArtsError;
