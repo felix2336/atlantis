42de79aaaf77668c34ab5762e9248d4922938daf
felix2336 <felix-pra@gmx.de>
2024-08-22 16:27:49 +0200

    CHANGE: Changed ContextMenuCommand getMessages
    Changed to Database support

--- src/ContextMenus/Messages/getMessages.ts
+++ src/ContextMenus/Messages/getMessages.ts
@@ -1,7 +1,6 @@
-import { ContextMenuCommandBuilder, UserContextMenuCommandInteraction, ApplicationCommandType, PermissionFlagsBits, GuildMember, CacheType } from 'discord.js'
-import { MessageUser } from 'contents';
+import { ContextMenuCommandBuilder, UserContextMenuCommandInteraction, ApplicationCommandType, PermissionFlagsBits, GuildMember, CacheType, EmbedBuilder, Colors } from 'discord.js'
 import { UserContextMenu } from 'dcbot'
-import { readFileSync } from 'fs'
+import Messages from '../../Schemas/messages'
 
 export default new UserContextMenu({
     data: new ContextMenuCommandBuilder()
@@ -15,14 +14,23 @@ export default new UserContextMenu({
             interaction.reply({ content: 'Du bist nicht im Serverteam und darfst diesen Befehl nicht nutzen', ephemeral: true })
             return
         }
-        const DB = JSON.parse(readFileSync('./JSON/messages.json', 'utf8')) as MessageUser[]
-        const UserData = DB.find(u => u.userid == target.user.id)
-        if (!UserData){
-            interaction.reply({ content: 'Dieser Nutzer hat keinen Eintrag in der Datenbank.', ephemeral: true })
-            return
+        const DBuser = await Messages.findOne({userId: target.user.id})
+        if(!DBuser) {
+            const embed = new EmbedBuilder({
+                title: 'Keine Nachrichten gefunden',
+                description: 'Der User hat keine Nachrichten gesendet',
+                color: 0x00ff00
+            })
+            await interaction.reply({embeds: [embed]})
+            return;
         }
-        const User = new MessageUser().assignData(UserData)
 
-        interaction.reply({ content: `${target} hat in dieser Woche bereits ${User.getTotalMessages()} Nachrichten gesendet!`, ephemeral: true })
+        const embed = new EmbedBuilder({
+            title: 'Gesendete Nachrichten',
+            description: `${target} hat bereits ${DBuser.messagesSent} nachrichten gesendet`,
+            color: Colors.Gold
+        })
+
+        await interaction.reply({ embeds: [embed] })
     },
 })
