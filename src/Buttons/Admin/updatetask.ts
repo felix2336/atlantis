ffcb9fe7fcca65109ef4f097fc5c02c17fba488f
felix_2336 <felix-pra@gmx.de>
2024-08-27 09:04:22 +0200

    "Fixed whitespace and added type parameter to ActionRowBuilder"

--- src/Buttons/Admin/updatetask.ts
+++ src/Buttons/Admin/updatetask.ts
@@ -1,17 +1,17 @@
 import { Button } from 'dcbot'
 import { ButtonInteraction, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } from 'discord.js'
 
-export default new Button( {
+export default new Button({
     id: 'task_update',
 
-    async execute(interaction: ButtonInteraction){
+    async execute(interaction: ButtonInteraction) {
         const embed = interaction.message.embeds[0]
         const userId = embed.fields[0].value.split('@')[1].split('>')[0]
 
-        if(interaction.user.id != userId) {
-            interaction.reply({content: 'Nur der User, der die Task geclaimt hat, kann ein Update posten', ephemeral: true})
+        if (interaction.user.id != userId) {
+            interaction.reply({ content: 'Nur der User, der die Task geclaimt hat, kann ein Update posten', ephemeral: true })
             return
-        }  
+        }
 
         const modal = new ModalBuilder({
             title: 'Update zur Task posten',
@@ -25,8 +25,7 @@ export default new Button( {
             customId: 'update',
             required: true
         })
-        const row = new ActionRowBuilder().addComponents(updateField)
-        //@ts-ignore
+        const row = new ActionRowBuilder<TextInputBuilder>().addComponents(updateField)
         modal.addComponents(row)
 
         await interaction.showModal(modal)
