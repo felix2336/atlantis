c81dec54715852cda15df53fe0e56ea33f718094
felix_2336 <felix-pra@gmx.de>
2024-08-27 14:59:16 +0200

    "Refactored ticket.ts: extracted modal builders into separate objects, removed unnecessary imports and variables, and reorganized code structure."

--- src/SelectMenus/Ticket/ticket.ts
+++ src/SelectMenus/Ticket/ticket.ts
@@ -1,19 +1,14 @@
-import { StringSelectMenuInteraction, ModalBuilder, TextInputBuilder, ActionRowBuilder, ActionRow, AnySelectMenuInteraction } from "discord.js";
+import { StringSelectMenuInteraction, ModalBuilder, TextInputBuilder, ActionRowBuilder } from "discord.js";
 import { StringSelectMenu } from 'dcbot'
 import { MyClient } from "contents";
 
-export default new StringSelectMenu<MyClient>( {
-    id: 'ticket',
-
-    async execute(interaction, client) {
-        switch (interaction.values[0]) {
-            case 'bewerben': {
-                const modal = new ModalBuilder({
-                    title: 'Team Bewerbung',
-                    customId: 'apply'
-                })
-
-                const job = new TextInputBuilder({
+const modals = {
+    bewerben: new ModalBuilder({
+        title: 'Team Bewerbung',
+        customId: 'apply',
+        components: [
+            new ActionRowBuilder<TextInputBuilder>().addComponents(
+                new TextInputBuilder({
                     customId: 'job',
                     label: 'Als was möchtest du dich bewerben?',
                     placeholder: 'Supporter, Designer, Moderator',
@@ -21,8 +16,9 @@ export default new StringSelectMenu<MyClient>( {
                     required: true,
                     style: 1
                 })
-
-                const experience = new TextInputBuilder({
+            ),
+            new ActionRowBuilder<TextInputBuilder>().addComponents(
+                new TextInputBuilder({
                     customId: 'experience',
                     label: 'Hast du schon Erfahrungen in diesem Bereich?',
                     placeholder: 'Ganze Sätze + Erfahrungen nennen',
@@ -30,135 +26,113 @@ export default new StringSelectMenu<MyClient>( {
                     required: true,
                     style: 2
                 })
-
-                const name = new TextInputBuilder({
+            ),
+            new ActionRowBuilder<TextInputBuilder>().addComponents(
+                new TextInputBuilder({
                     customId: 'name',
                     label: 'Wie heißt du?',
                     maxLength: 255,
                     required: true,
                     style: 1
                 })
-
-                const age = new TextInputBuilder({
+            ),
+            new ActionRowBuilder<TextInputBuilder>().addComponents(
+                new TextInputBuilder({
                     customId: 'age',
                     label: 'Wie alt bist du?',
                     required: true,
                     maxLength: 255,
                     style: 1,
-
                 })
-
-                const onlinetime = new TextInputBuilder({
+            ),
+            new ActionRowBuilder<TextInputBuilder>().addComponents(
+                new TextInputBuilder({
                     customId: 'onlinetime',
                     label: 'Bitte nenne uns deine Discord-Onlinezeiten:',
                     style: 2,
                     required: true,
                     maxLength: 1024
                 })
-
-
-                const row = new ActionRowBuilder<TextInputBuilder>().addComponents(job)
-                const row2 = new ActionRowBuilder<TextInputBuilder>().addComponents(experience)
-                const row3 = new ActionRowBuilder<TextInputBuilder>().addComponents(name)
-                const row4 = new ActionRowBuilder<TextInputBuilder>().addComponents(age)
-                const row5 = new ActionRowBuilder<TextInputBuilder>().addComponents(onlinetime)
-
-                modal.addComponents(row, row2, row3, row4, row5)
-                await interaction.showModal(modal)
-                break
-            }
-
-            case ('partnerschaft'): {
-                const modal = new ModalBuilder({
-                    title: 'Partnerschaft',
-                    customId: 'partnerschaft',
-                    components: [
-                        new ActionRowBuilder<TextInputBuilder>({
-                            components: [
-                                new TextInputBuilder({
-                                    label: 'Wie viele Mitglieder hat dein Server?',
-                                    customId: 'membercount',
-                                    required: true,
-                                    style: 1,
-                                })
-                            ]
-                        }),
-                        new ActionRowBuilder<TextInputBuilder>({
-                            components: [
-                                new TextInputBuilder({
-                                    label: 'Bitte beschreibe deinen Server',
-                                    customId: 'description',
-                                    required: true,
-                                    minLength: 20,
-                                    placeholder: 'Bitte schreibe ganze Sätze',
-                                    style: 2
-                                })
-                            ]
-                        })
-                    ]
+            ),
+        ]
+    }),
+    partnerschaft: new ModalBuilder({
+        title: 'Partnerschaft',
+        customId: 'partnerschaft',
+        components: [
+            new ActionRowBuilder<TextInputBuilder>().addComponents(
+                new TextInputBuilder({
+                    label: 'Wie viele Mitglieder hat dein Server?',
+                    customId: 'membercount',
+                    required: true,
+                    style: 1,
                 })
-                await interaction.showModal(modal)
-
-                break
-            }
-
-            case 'report': {
-                const modal = new ModalBuilder({
-                    title: 'Person melden',
-                    customId: 'report',
-                    components: [
-                        new ActionRowBuilder<TextInputBuilder>().addComponents([
-                            new TextInputBuilder({
-                                customId: 'target',
-                                label: 'wie heisst die person?',
-                                style: 1,
-                                placeholder: 'Bitte nenne uns seinen Discord-Namen',
-                                required: true
-                            })
-                        ]),
-                        new ActionRowBuilder<TextInputBuilder>().addComponents([
-                            new TextInputBuilder({
-                                customId: 'reason',
-                                label: 'Was hat die Person gemacht?',
-                                style: 2,
-                                maxLength: 1024,
-                                placeholder: 'Bitte schreibe ganze Sätze'
-                            })
-                        ]),
-                        new ActionRowBuilder<TextInputBuilder>().addComponents([
-                            new TextInputBuilder({
-                                label: 'Hast du Beweise',
-                                customId: 'evidence',
-                                placeholder: 'Ja/Nein',
-                                style: 1
-                            })
-                        ])
-                    ]
+            ),
+            new ActionRowBuilder<TextInputBuilder>().addComponents(
+                new TextInputBuilder({
+                    label: 'Bitte beschreibe deinen Server',
+                    customId: 'description',
+                    required: true,
+                    minLength: 20,
+                    placeholder: 'Bitte schreibe ganze Sätze',
+                    style: 2
                 })
-                await interaction.showModal(modal)
-                break
-            }
-
-            case 'support': {
-                const modal = new ModalBuilder({
-                    title: 'Allgemeiner Support',
-                    customId: 'support',
-                    components: [
-                        new ActionRowBuilder<TextInputBuilder>().addComponents([
-                            new TextInputBuilder({
-                                label: 'Wie lautet dein Anliegen?',
-                                customId: 'issue',
-                                required: true,
-                                maxLength: 1024,
-                                placeholder: 'Bitte schreibe ganze Sätze!',
-                                style: 2
-                            })
-                        ])
-                    ]
+            )
+        ]
+    }),
+    report: new ModalBuilder({
+        title: 'Person melden',
+        customId: 'report',
+        components: [
+            new ActionRowBuilder<TextInputBuilder>().addComponents(
+                new TextInputBuilder({
+                    customId: 'target',
+                    label: 'wie heisst die person?',
+                    style: 1,
+                    placeholder: 'Bitte nenne uns seinen Discord-Namen',
+                    required: true
                 })
-                await interaction.showModal(modal)
-                break
-            }
-        }
+            ),
+            new ActionRowBuilder<TextInputBuilder>().addComponents(
+                new TextInputBuilder({
+                    customId: 'reason',
+                    label: 'Was hat die Person gemacht?',
+                    style: 2,
+                    maxLength: 1024,
+                    placeholder: 'Bitte schreibe ganze Sätze'
+                })
+            ),
+            new ActionRowBuilder<TextInputBuilder>().addComponents(
+                new TextInputBuilder({
+                    label: 'Hast du Beweise',
+                    customId: 'evidence',
+                    placeholder: 'Ja/Nein',
+                    style: 1
+                })
+            )
+        ]
+    }),
+    support: new ModalBuilder({
+        title: 'Allgemeiner Support',
+        customId: 'support',
+        components: [
+            new ActionRowBuilder<TextInputBuilder>().addComponents(
+                new TextInputBuilder({
+                    label: 'Wie lautet dein Anliegen?',
+                    customId: 'issue',
+                    required: true,
+                    maxLength: 1024,
+                    placeholder: 'Bitte schreibe ganze Sätze!',
+                    style: 2
+                })
+            )
+        ]
+    })
+}
+
+export default new StringSelectMenu<MyClient>({
+    id: 'ticket',
+    async execute(interaction, client) {
+        await interaction.showModal(modals[interaction.values[0]])
     }
 })
