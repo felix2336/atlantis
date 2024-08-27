cb3bf1b1bc64b4716daf85e602cc98aa250ce618
felix_2336 <felix-pra@gmx.de>
2024-08-27 09:03:06 +0200

    "Fixed syntax errors and added type parameter to ActionRowBuilder"

--- src/Buttons/Admin/claimtask.ts
+++ src/Buttons/Admin/claimtask.ts
@@ -1,7 +1,7 @@
 import { Button } from 'dcbot'
 import { ButtonInteraction, EmbedBuilder, ActionRowBuilder, ButtonBuilder, GuildMember } from 'discord.js'
 
-export default new Button ({
+export default new Button({
     id: 'taskclaim',
 
     async execute(interaction: ButtonInteraction) {
@@ -9,17 +9,17 @@ export default new Button ({
         if (!member.roles.cache.has('1146117778483450048')) {
             interaction.reply({ content: 'Dazu bist du nicht berechtigt', ephemeral: true })
             return
-        } 
+        }
 
         const message = await interaction.message.fetch()
         if (!message) {
             interaction.reply({ content: 'Ein unerwarteter Fehler ist aufgetreten', ephemeral: true })
             return
-        } 
+        }
 
         message.components[0].components.pop()
 
-        const row = new ActionRowBuilder().addComponents([
+        const row = new ActionRowBuilder<ButtonBuilder>().addComponents([
             new ButtonBuilder({
                 label: 'Erledigt',
                 customId: 'taskfinished',
@@ -32,9 +32,9 @@ export default new Button ({
             })
         ])
 
+
         const embed = new EmbedBuilder(message.embeds[0].data)
         embed.setFields([{ name: 'Wird bearbeitet von', value: `${interaction.user}` }])
-        //@ts-ignore
         await message.edit({ embeds: [embed], components: [row] })
         interaction.reply({ content: 'Du hast diese Aufgabe erfolgreich geclaimt', ephemeral: true })
     }
