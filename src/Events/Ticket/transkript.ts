6398617edb78b381ceae5130639c78592bed2c96
felix_2336 <felix-pra@gmx.de>
2024-08-29 14:43:42 +0200

    "Refactor transkript event handler: removed unnecessary variables, added semicolons, and reformatted code"

--- src/Events/Ticket/transkript.ts
+++ src/Events/Ticket/transkript.ts
@@ -6,28 +6,25 @@ export default new Event({
     name: Events.MessageCreate,
 
     async execute(client, message: Message) {
-        if(message.author.bot) return;
-        const channel = message.channel as TextChannel
-        if (channel.parentId != Categories.ticket) return;
-        const transkripts = client.channels.cache.get(Channels.ticket_transkripts) as ForumChannel
-        const wh = (await transkripts.fetchWebhooks()).first()
-        const transkript = transkripts.threads.cache.find(ch => ch.name == channel.name)
+        if (message.author.bot) return;
+        const channel = message.channel as TextChannel;
+        if (channel.parentId !== Categories.ticket) return;
 
-        if(!wh) {
-            return
-        }
+        const transkripts = client.channels.cache.get(Channels.ticket_transkripts) as ForumChannel;
+        const wh = (await transkripts.fetchWebhooks()).first();
+        if (!wh) return;
 
-        const attachments: Attachment[] = []
-        for(const [_, attachment] of message.attachments) {
-            attachments.push(attachment)
-        }
+        const transkript = transkripts.threads.cache.find(ch => ch.name === channel.name);
+        if (!transkript) return;
+
+        const attachments = Array.from(message.attachments.values());
 
         await wh.send({
             files: attachments,
             content: message.content,
-            threadId: transkript!.id,
+            threadId: transkript.id,
             username: message.author.username,
             avatarURL: message.author.displayAvatarURL(),
-        })
+        });
     },
 })
