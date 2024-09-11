441f54b55cd617a4842273c0cf49c3db2cd6c094
felix2336 <felix-pra@gmx.de>
2024-09-11 21:15:51 +0200

    "Added fortune command"

---
+++ src/Commands/General/fortune.ts
@@ -0,0 +1,31 @@
+import { SlashCommand } from "dcbot";
+import { SlashCommandBuilder, EmbedBuilder, Colors } from "discord.js";
+import {exec} from 'child_process'
+
+export default new SlashCommand({
+    data: new SlashCommandBuilder()
+        .setName('fortune')
+        .setDescription('Öffne einen Glückskeks'),
+
+    async execute(interaction, client) {
+        exec('fortune', async (error, stdout, stderr) => {
+            if(error) {
+                client.logger.error(error.message)
+                const embed = new EmbedBuilder({
+                    title: 'Unerwarteter Fehler',
+                    description: 'Ein Fehler ist aufgetreten. Bitte kontaktiere einen Administrator.',
+                    color: Colors.DarkRed
+                })
+                await interaction.reply({embeds: [embed]})
+            }
+            if(stdout) {
+                const embed = new EmbedBuilder({
+                    title: 'Glückskeks',
+                    description: '***Du öffnest einen Glückskeks und erhältst folgende Nachricht:***\n' + stdout,
+                    color: Colors.Green
+                })
+                await interaction.reply({embeds: [embed]})
+            }
+        })
+    },
+})
