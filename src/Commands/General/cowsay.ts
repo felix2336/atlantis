4d99041eccb5cdf9e8a638b30324ec097dbb7242
felix2336 <felix-pra@gmx.de>
2024-09-11 21:23:10 +0200

    Added cowsay Command

---
+++ src/Commands/General/cowsay.ts
@@ -0,0 +1,32 @@
+import { SlashCommand } from "dcbot";
+import { SlashCommandBuilder, EmbedBuilder, Colors } from "discord.js";
+import {exec} from 'child_process'
+
+export default new SlashCommand({
+    data: new SlashCommandBuilder()
+        .setName('cowsay')
+        .setDescription('Lasse eine Kuh etwas sagen')
+        .addStringOption(input => input.setName('input').setDescription('Gib hier den Satz ein, den die Kuh sagen soll').setRequired(true)),
+
+    async execute(interaction, client) {
+        const input = interaction.options.getString('input', true);
+        exec(`cowsay ${input}`, async (error, stdout, stderr) => {
+            if(error) {
+                const embed = new EmbedBuilder({
+                    title: 'Fehler',
+                    description: 'Ein Fehler ist aufgetreten. Bitte wende dich an einen Administrator',
+                    color: Colors.DarkRed
+                })
+                await interaction.reply({embeds: [embed]})
+            }
+            if(stdout) {
+                const embed = new EmbedBuilder({
+                    title: 'Kuh sagt',
+                    description: `\`\`\`\n${stdout}\n\`\`\``,
+                    color: Colors.Green
+                })
+                await interaction.reply({embeds: [embed]})
+            }
+        })
+    },
+})
