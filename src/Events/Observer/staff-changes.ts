087d20b95b23be5864bc76d6f3e7344ad1fdd9fa
felix2336 <felix-pra@gmx.de>
2024-10-19 01:27:52 +0200

    Removed deleted roles in staff list emded

--- src/Events/Observer/staff-changes.ts
+++ src/Events/Observer/staff-changes.ts
@@ -11,21 +11,16 @@ export default new Event({
     const staff: string[] = [
       '1170957646942191688', //Eigentümer
       '1181259236408311828', //Serverleitung
-      '1196522075691679754', //Verwaltung-Management
       '1146117778483450048', //Dev
       '1146113684570124344', //Admin
       '1236722805660324033', //Mod Leitung
       '1147206142548787372', //mod
-      '1237032478196236380', //test mod
       '1236722501115969586', //Sup Leitung
       '1148217519631499384', //sup
       '1146113684570124342', //test sup
       '1201591226143608912', //Marketing
       '1201591186272555150', //Test marketing
-      '1228063213288161340', //marketing content creator
-      '1228063079774949406', //test marketing content creator
-      '1180773957708820551', //Server-Management
-      '1216758777290690710', //Event Team
+      '1216758777290690710', //Werbe Team
     ]
     const message = await channel.messages.fetch('1227354357452898377')
 
